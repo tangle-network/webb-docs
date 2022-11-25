90889518e593445d246b944cdff03fb546ae89ca
Dustin Brickwood <dustinbrickwood204@gmail.com>
2022-11-24 21:08:57 -0600

    chore: updates

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,8 +4,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  environment: process.env.NEXT_PUBLIC_VERCEL_ENV,
-  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: process.env.SENTRY_DSN, 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
 });
