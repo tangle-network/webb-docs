355eec899930190dabbe64d6867d1031ce63f32d
Dustin Brickwood <dustinbrickwood204@gmail.com>
2022-12-14 18:39:28 -0700

    chore: deposit updates

--- components/images/bridge/usage-guide/deposits/Deposits.tsx
+++ components/images/bridge/usage-guide/deposits/Deposits.tsx
@@ -1,39 +1,46 @@
 import Image from 'next/image'
-import ConnectWalletDark from './connect-wallet-dark.png';
-import ConnectWalletLight from './connect-wallet-light.png';
-import ConnectedWalletDark from './connected-wallet-dark.png';
-import ConnectedWalletLight from './connected-wallet-light.png';
-import SourceChainLight from './select-source-chain-light.png';
-import SourceChainDark from './select-source-chain-dark.png';
-import WalletComponentLight from './wallet-component-light.png';
-import WalletComponentDark from './wallet-component-dark.png';
-import CreateAccountLight from './create-note-account-light.png';
-import CreateAccountDark from './create-note-account-dark.png';
-import CreateAccountPromptLight from './create-account-prompt-light.png';
-import CreateAccountPromptDark from './create-account-prompt-dark.png';
-import SignatureRequest from './signature-request-mm.png';
+import TokenSelectionLight from './select-token-light.png';
+import TokenSelectionDark from './select-token-dark.png';
+import DepositLight from './deposit-light.png';
+import DepositDark from './deposit-dark.png';
+import WrapDepositDark from './wrap-deposit-dark.png';
+import SourceChainLight from '../account/select-source-chain-light.png';
+import SourceChainDark from '../account/select-source-chain-dark.png';
 
-export const ConnectWallet = (props) => {
+export const Deposit = () => {
   return (
     <>
     <div className="block dark:hidden">
-      <Image src={ConnectWalletLight} height={props.height} alt="connect wallet light" />
+      <Image src={DepositLight} alt="deposit light" />
     </div>
     <div className="hidden dark:block">
-      <Image src={ConnectWalletDark} height={props.height} alt="connect wallet dark" />
+      <Image src={DepositDark}alt="deposit dark" />
     </div>
     </>
   )
 };
 
-export const ConnectedWallet = () => {
+export const TokenSelection = (props) => {
+  return (
+    <>
+    <div className="block dark:hidden">
+      <Image src={TokenSelectionLight} height={props.height} alt="token selection light" />
+    </div>
+    <div className="hidden dark:block">
+      <Image src={TokenSelectionDark} height={props.height} alt="token selection dark" />
+    </div>
+    </>
+  )
+};
+
+export const WrapDeposit = () => {
     return (
       <>
       <div className="block dark:hidden">
-        <Image src={ConnectedWalletLight} alt="connected wallet light" />
+        <Image src={WrapDepositDark} alt="connected wallet light" />
       </div>
       <div className="hidden dark:block">
-        <Image src={ConnectedWalletDark}alt="connected wallet dark" />
+        <Image src={WrapDepositDark}alt="wrap and deposit dark" />
       </div>
       </>
     )
@@ -47,55 +54,6 @@ export const SelectSourceChain = () => {
       </div>
       <div className="hidden dark:block">
         <Image src={SourceChainDark}alt="connected wallet dark" />
-      </div>
-      </>
-    )
-};
-
-export const WalletComponent = () => {
-    return (
-      <>
-      <div className="block dark:hidden">
-        <Image src={WalletComponentLight} alt="wallet light" />
-      </div>
-      <div className="hidden dark:block">
-        <Image src={WalletComponentDark}alt="wallet dark" />
-      </div>
-      </>
-    )
-};
-
-export const CreateNoteAccount = () => {
-    return (
-      <>
-      <div className="block dark:hidden">
-        <Image src={CreateAccountLight} alt="create account light" />
-      </div>
-      <div className="hidden dark:block">
-        <Image src={CreateAccountDark}alt="create account dark" />
-      </div>
-      </>
-    )
-};
-
-export const CreateNoteAccountPrompt = () => {
-    return (
-      <>
-      <div className="block dark:hidden">
-        <Image src={CreateAccountPromptLight} alt="create account prompt light" />
-      </div>
-      <div className="hidden dark:block">
-        <Image src={CreateAccountPromptDark}alt="create account prompt dark" />
-      </div>
-      </>
-    )
-};
-
-export const SignatureRequestPrompt = () => {
-    return (
-      <>
-      <div>
-        <Image src={SignatureRequest} alt="signature request prompt light" />
       </div>
       </>
     )
