f1fe40925a26cbe22981666bde61504e0a44690b
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-03-17 08:53:45 -0500

    chore: adds latex support

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 const { withSentryConfig } = require("@sentry/nextjs");
+const remarkMath = require('remark-math')
+const rehypeKatex = require('rehype-katex')
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
   unstable_flexsearch: true,
   unstable_staticImage: true,
+  latex: true,
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
 
 const sentryWebpackPluginOptions = {
