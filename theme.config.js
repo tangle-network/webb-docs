5399750f67ddd82fdcca03258e2b0c8101a197f3
Thom Ivy <38070512+thomivy@users.noreply.github.com>
2023-06-20 10:54:36 -0500

    Restructuring branch (#110)

--- theme.config.js
+++ theme.config.js
@@ -43,7 +43,7 @@ const theme = {
       router.asPath === "/" ? SITE_ROOT : `${SITE_ROOT}${router.asPath}`;
 
     const asPath = router.asPath;
-    
+
     return (
       <>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -81,8 +81,14 @@ const theme = {
         <meta property="og:type" content="website" />
         <meta property="og:url" content={fullUrl} />
         <link rel="canonical" href={fullUrl} />
-        <meta property="twitter:image" content="https://webb-assets.s3.amazonaws.com/WebbLogo.svg" />
-        <meta property="og:image" content="https://webb-assets.s3.amazonaws.com/WebbLogo.svg" />
+        <meta
+          property="twitter:image"
+          content="https://webb-assets.s3.amazonaws.com/WebbLogo.svg"
+        />
+        <meta
+          property="og:image"
+          content="https://webb-assets.s3.amazonaws.com/WebbLogo.svg"
+        />
         <meta property="og:locale" content="en_IE" />
         <meta property="og:site_name" content="Webb" />
         <link rel="prefetch" href="/" as="document" />
@@ -91,6 +97,9 @@ const theme = {
   },
   editLink: {
     text: "Edit this page on GitHub",
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: "1",
   },
   navbar: {
     component: Navigation,
