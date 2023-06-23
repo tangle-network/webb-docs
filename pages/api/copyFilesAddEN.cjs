6e860bd5dbcda9ce155a8ffa560cb3e8f57fa9d6
Thom Ivy <38070512+thomivy@users.noreply.github.com>
2023-06-22 19:13:34 -0500

    repaired netlify build errors- path issues

--- pages/api/copyFilesAddEN.cjs
+++ pages/api/copyFilesAddEN.cjs
@@ -1,11 +1,11 @@
 // This script copies all existing mdx and json filed in /pages/docs and appends an .en in the extension, to prepare for internationalization.
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function copyFilesToDirectory(sourceDirectory, targetDirectory) {
   // Ensure target directory exists
-  if (!fs.existsSync(targetDirectory)){
+  if (!fs.existsSync(targetDirectory)) {
     fs.mkdirSync(targetDirectory, { recursive: true });
   }
 
@@ -17,12 +17,21 @@ function copyFilesToDirectory(sourceDirectory, targetDirectory) {
 
     if (stats.isDirectory()) {
       copyFilesToDirectory(oldPath, path.join(targetDirectory, file));
-    } else if (stats.isFile() && (file.endsWith('.mdx') || file.endsWith('.json'))) {
-      const newPath = path.join(targetDirectory, file).replace(/\.mdx$/, '.en.mdx').replace(/\.json$/, '.en.json');
+    } else if (
+      stats.isFile() &&
+      (file.endsWith(".mdx") || file.endsWith(".json"))
+    ) {
+      const newPath = path
+        .join(targetDirectory, file)
+        .replace(/\.mdx$/, ".en.mdx")
+        .replace(/\.json$/, ".en.json");
       fs.copyFileSync(oldPath, newPath);
       console.log(`Copied ${oldPath} to ${newPath}`);
     }
   }
 }
 
-copyFilesToDirectory(path.join(process.cwd(), 'pages/docs'), path.join(process.cwd(), 'docs-en'));
+copyFilesToDirectory(
+  path.join(process.cwd(), "pages/docs"),
+  path.join(process.cwd(), "docs-en")
+);
