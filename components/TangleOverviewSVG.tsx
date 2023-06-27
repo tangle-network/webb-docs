d60647ddbfaa45373bfb53b9afcb7d9c74beb423
Thom Ivy <38070512+thomivy@users.noreply.github.com>
2023-06-27 17:51:14 -0500

    Ran Prettier

--- components/TangleOverviewSVG.tsx
+++ components/TangleOverviewSVG.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 const SvgComponent = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -1261,5 +1261,5 @@ const SvgComponent = (props) => (
       />
     </defs>
   </svg>
-)
-export default SvgComponent
+);
+export default SvgComponent;
