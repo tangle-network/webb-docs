464a4bded892caad9b90067e6e6f7ce026e462ed
Thom Ivy <38070512+thomivy@users.noreply.github.com>
2023-10-11 08:29:02 -0500

    manually prettiered in attempt to fix netlify deploy bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -471,7 +471,7 @@ export function Footer({ menu }: { menu?: boolean }): ReactElement {
       <div
         className={cn(
           "mx-auto max-w-[90rem] py-2 px-4 flex gap-2",
-          menu ? "flex" : "hidden",
+          menu ? "flex" : "hidden"
         )}
       >
         <ThemeSwitch />
@@ -480,7 +480,7 @@ export function Footer({ menu }: { menu?: boolean }): ReactElement {
       <div
         className={cn(
           "mx-auto max-w-[90rem] py-12 flex justify-center md:justify-center text-black dark:text-white",
-          "pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]",
+          "pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]"
         )}
       >
         <FooterContent />
